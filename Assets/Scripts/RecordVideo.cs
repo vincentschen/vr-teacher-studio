de490ee6adaaad9df4e84c729a334472ee02c583
Vincent Chen <vincentschen@yahoo.com>
2017-04-29 20:49:53 -0700

    Add RecordVideo script, which creates screenshots of every frame from secondary camera

---
+++ Assets/Scripts/RecordVideo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Reference: http://wiki.unity3d.com/index.php/ScreenShotMovie
+
+public class RecordVideo : MonoBehaviour {
+
+    // The folder we place all screenshots inside.
+    // If the folder exists we will append numbers to create an empty folder.
+    public string folder = "ScreenshotMovieOutput";
+    public int frameRate = 25;
+    public int sizeMultiplier = 1;
+
+    private string realFolder = "";
+
+    void Start()
+    {
+        // Set the playback framerate!
+        // (real time doesn't influence time anymore)
+        Time.captureFramerate = frameRate;
+
+        // Find a folder that doesn't exist yet by appending numbers!
+        realFolder = folder;
+        int count = 1;
+        while (System.IO.Directory.Exists(realFolder))
+        {
+            realFolder = folder + count;
+            count++;
+        }
+        // Create the folder
+        System.IO.Directory.CreateDirectory(realFolder);
+    }
+
+    void Update()
+    {
+        // name is "realFolder/shot 0005.png"
+        var name = string.Format("{0}/shot {1:D04}.png", realFolder, Time.frameCount);
+
+        // Capture the screenshot
+        Application.CaptureScreenshot(name, sizeMultiplier);
+    }
+}
