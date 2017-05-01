85e54372f701b51df3ac370daca41e2d7740f4a2
Vincent Chen <vincentschen@yahoo.com>
2017-04-30 18:48:58 -0700

    Add line rendering based on global position of touch controller

--- Assets/Scripts/DrawLineManager.cs
+++ Assets/Scripts/DrawLineManager.cs
@@ -8,6 +8,7 @@ public class DrawLineManager : MonoBehaviour {
     private int prevTriggerState; 
 
     private LineRenderer currLine;
+    private int numClicks = 0;
 
 	// Update is called once per frame
 	void Update () {
@@ -15,18 +16,34 @@ public class DrawLineManager : MonoBehaviour {
         float rightTriggerVal = OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger);
         //Debug.Log(rightTriggerVal);
 
-        // right trigger
+        // Right Trigger Enter
         if (rightTriggerVal > 0 && rightTriggerVal < 0.35f 
             && prevTriggerState == (int)triggerStates.standby)
         {
-            Debug.Log("enter");
-            prevTriggerState = (int)triggerStates.enter; 
+            prevTriggerState = (int)triggerStates.enter;
+
+            GameObject go = new GameObject ();
+            currLine = go.AddComponent<LineRenderer>();
+
+            numClicks = 0;
         }
+
+        // Right Trigger Hold 
         else if (rightTriggerVal > 0.35f)
         {
-            prevTriggerState = (int)triggerStates.hold; 
-            Debug.Log("hold");
+            prevTriggerState = (int)triggerStates.hold;
+
+            currLine.SetVertexCount(numClicks + 1);
+
+            // global position of right controller
+            Vector3 rightControllerPos = GameObject.Find("OVRCameraRig").transform.position
+                + OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTrackedRemote);
+            currLine.SetPosition(numClicks, rightControllerPos);
+
+            numClicks++; 
         }
+
+        // Right Trigger Standby
         else if (rightTriggerVal == 0)
         {
             prevTriggerState = (int)triggerStates.standby; 
