4887bb0b45f961f49d52666c5d2a42bbe4c6db12
Vincent Chen <vincentschen@yahoo.com>
2017-05-01 11:08:40 -0700

    Add preview video behind record camera

---
+++ Assets/Resources/SecurityCamera/Scripts/CameraMove.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+    public Transform securityCamera;
+
+    private Vector3 securityCameraStartPos;
+
+    private GameObject vampire;
+
+    void Start()
+    {
+        securityCameraStartPos = securityCamera.position;
+        vampire = GameObject.Find("Vampire");
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            securityCamera.position = (Vector3)securityCamera.position + new Vector3(-0.1f, 0,0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            securityCamera.position = (Vector3)securityCamera.position + new Vector3(0.1f, 0,0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            securityCamera.position = (Vector3)securityCamera.position + new Vector3(0,0.1f,0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            securityCamera.position = (Vector3)securityCamera.position + new Vector3(0,-0.1f,0);
+        }
+
+	    if (Input.GetKeyDown(KeyCode.V))
+	    {
+	        ToggleVampire();
+	    }
+
+
+	   }
+
+    void ToggleVampire()
+    {
+        if (vampire.layer == 8)
+        {
+            vampire.layer = 0;
+        }
+        else
+        {
+            vampire.layer = 8;
+        }
+    }
+}
---
+++ Assets/Resources/SecurityCamera/Scripts/CameraSnapshot.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CameraSnapshot : MonoBehaviour
+{
+    [SerializeField]
+    RenderTexture securityCameraTexture;  // drag the render texture onto this field in the Inspector
+    [SerializeField]
+    Camera securityCamera; // drag the security camera onto this field in the inspector
+
+    void LateUpdate()
+    {
+
+        if (Input.GetKeyDown("s"))
+        {
+            StartCoroutine(SaveCameraView());
+        }
+
+    }
+
+
+    public IEnumerator SaveCameraView()
+    {
+        yield return new WaitForEndOfFrame();
+
+        // get the camera's render texture
+        RenderTexture rendText = RenderTexture.active;
+        RenderTexture.active = securityCamera.targetTexture;
+
+        // render the texture
+        securityCamera.Render();
+
+        // create a new Texture2D with the camera's texture, using its height and width
+        Texture2D cameraImage = new Texture2D(securityCamera.targetTexture.width, securityCamera.targetTexture.height, TextureFormat.RGB24, false);
+        cameraImage.ReadPixels(new Rect(0, 0, securityCamera.targetTexture.width, securityCamera.targetTexture.height), 0, 0);
+        cameraImage.Apply();
+        RenderTexture.active = rendText;
+
+        // store the texture into a .PNG file
+        byte[] bytes = cameraImage.EncodeToPNG();
+
+
+        // save the encoded image to a file
+        System.IO.File.WriteAllBytes(Application.persistentDataPath + "/camera_image.png", bytes);
+    }
+}
